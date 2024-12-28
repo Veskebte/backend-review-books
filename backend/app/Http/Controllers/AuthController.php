a65b5aa746005ec91b23a45971dab46bfb4fecc7
Veskebte <nathasyapernidyaashari_2327250063@mhs.mdp.ac.id>
2024-12-28 13:54:55 +0700

    Login Register API

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $validate = $request->validate([
+            'name' => 'required',
+            'username' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'password_confirmation' => 'required|same:password',
+        ]);
+
+        $validate['password'] =  bcrypt($request->password);
+        $validate['role'] = 'user';
+
+        $user = User::create($validate);
+        $success['token'] = $user->createToken('AdminBooklist')->plainTextToken;
+        $success['name'] = $user->name;
+
+        return response()->json($success, Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request) {
+        if (Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password,
+        ])) {
+            $user = Auth::user();
+            $success['token'] = $user->createToken('AdminBooklist')->plainTextToken;
+            $success['name'] = $user->name;
+            $success['role'] = $user->role;
+            return response()->json($success, 201);
+        } else {
+            return response()->json(['error' => 'Akun tidak terdaftar'], 401);
+        }
+    }
+}
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -19,8 +20,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'username',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -32,7 +35,7 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
-
+    
     /**
      * Get the attributes that should be cast.
      *
